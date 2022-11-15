b235d3847cabfd3cbf3474c8eab7c81b47d49913
Said Al-Ghiyats <said.ghiyats@gmail.com>
2022-11-15 15:39:32 +0700

    revisi

--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -3,11 +3,8 @@ import { useState, createContext } from 'react';
 export const Context = createContext();
 export const ContextProvider = ({ children }) => {
   const [open, setOpen] = useState(true);
-  const [showBookmark, setShowBookmark] = useState(true);
 
   return (
-    <Context.Provider value={{ open, setOpen, showBookmark, setShowBookmark }}>
-      {children}
-    </Context.Provider>
+    <Context.Provider value={{ open, setOpen }}>{children}</Context.Provider>
   );
 };
--- src/pages/surah/[surah].jsx
+++ src/pages/surah/[surah].jsx
@@ -5,16 +5,11 @@ import NavList from '../../components/NavList';
 import PrevNext from '../../components/PrevNext';
 import ForClose from '../../components/ForClose';
 import BtnNavList from '../../components/BtnNavList';
-import { useState, useContext } from 'react';
-import useBookmarks from './useBookmarks';
-import { Context } from '../../contexts/Context';
-import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Surah({ data1, data2, data3 }) {
   const [showModal, setShowModal] = useState(true);
   const [nomor, setNomor] = useState('');
-  const [bookmarks, toggleBookmark] = useBookmarks();
-  const { showBookmark, setShowBookmark } = useContext(Context);
 
   const Tafsir = (id) => {
     setShowModal(false);
@@ -35,59 +30,6 @@ export default function Surah({ data1, data2, data3 }) {
 
       <NavList data1={data1} data2={data2} />
       <ForClose data3={data3} />
-
-      <div
-        className={`${
-          showBookmark ? 'invisible opacity-0' : 'opacity-100 visible'
-        } z-[60] fixed top-0 left-0 w-full h-full outline-none overflow-x-hidden overflow-y-auto supports-[backdrop-filter]:backdrop-saturate-[180%] supports-[backdrop-filter]:backdrop-blur-[10px]`}>
-        <div className="modal-dialog modal-dialog-scrollable relative w-auto pointer-events-none">
-          <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto dark:bg-slate-900 bg-white bg-clip-padding rounded-md outline-none text-current">
-            <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b dark:border-slate-50/[0.06] border-gray-200 rounded-t-md">
-              <h5 className="text-xl font-medium leading-normal ">Bookmarks</h5>
-              <span
-                onClick={() => setShowBookmark(true)}
-                className="cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={24}
-                  height={24}
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="hover:stroke-sky-400">
-                  <line x1={18} y1={6} x2={6} y2={18} />
-                  <line x1={6} y1={6} x2={18} y2={18} />
-                </svg>
-              </span>
-            </div>
-            <div id="theModal" className="modal-body relative p-4 text-justify">
-              {data1.ayat
-                ?.filter((s) => bookmarks?.includes(s.id))
-                .map((e) => {
-                  return (
-                    <div key={s.nomor}>
-                      <Link href={'/surah/' + e.surah + `#${e.nomor}`}>
-                        <p>{data3.nama_latin}</p>
-                        <p>{e.nomor}</p>
-                      </Link>
-                    </div>
-                  );
-                })}
-            </div>
-            <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 dark:border-slate-50/[0.06] rounded-b-md">
-              <button
-                onClick={() => setShowBookmark(true)}
-                type="button"
-                className="inline-block px-6 py-2.5 bg-sky-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-700 hover:shadow-lg focus:bg-sky-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-800 active:shadow-lg transition duration-150 ease-in-out">
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <div
         className={`${
@@ -143,7 +85,6 @@ export default function Surah({ data1, data2, data3 }) {
               <HeadSurah data1={data1} />
               <PrevNext data1={data1} />
               {data1.ayat.map((isi) => {
-                const isBookmarked = bookmarks?.includes(isi.id);
                 return (
                   <div
                     key={isi.nomor}
@@ -152,51 +93,6 @@ export default function Surah({ data1, data2, data3 }) {
                     <div className="pb-6 flex justify-between">
                       <span className="text-sm">{isi.nomor}</span>
                       <div className="flex gap-4">
-                        <span
-                          onClick={toggleBookmark(isi.id)}
-                          className="cursor-pointer duration-200">
-                          <svg
-                            className={` hover:stroke-sky-400`}
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                            width={18}
-                            height={18}
-                            viewBox="0 0 24 24">
-                            <g transform="translate(4.500000, 2.500000)">
-                              <path d="M7.47024319,0 C1.08324319,0 0.00424318741,0.932 0.00424318741,8.429 C0.00424318741,16.822 -0.152756813,19 1.44324319,19 C3.03824319,19 5.64324319,15.316 7.47024319,15.316 C9.29724319,15.316 11.9022432,19 13.4972432,19 C15.0932432,19 14.9362432,16.822 14.9362432,8.429 C14.9362432,0.932 13.8572432,0 7.47024319,0 Z" />
-                              {isBookmarked ? (
-                                <line
-                                  className="fill-none"
-                                  transform="translate(-4.500000, -2.500000)"
-                                  x1={15}
-                                  x2={9}
-                                  y1={9}
-                                  y2={9}
-                                />
-                              ) : (
-                                <>
-                                  <line
-                                    className={`fill-none `}
-                                    transform="translate(-4.500000, -2.500000)"
-                                    x1={12}
-                                    x2={12}
-                                    y1={6}
-                                    y2={12}
-                                  />
-                                  <line
-                                    className="fill-none"
-                                    transform="translate(-4.500000, -2.500000)"
-                                    x1={15}
-                                    x2={9}
-                                    y1={9}
-                                    y2={9}
-                                  />
-                                </>
-                              )}
-                            </g>
-                          </svg>
-                        </span>
                         <span
                           onClick={() => Tafsir(isi.nomor)}
                           className="cursor-pointer duration-200">
